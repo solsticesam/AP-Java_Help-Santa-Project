584fe8ec106fc854d7c0e940abaf0c28899f1359
solsticesam <44234583+solsticesam@users.noreply.github.com>
2018-12-10 12:54:32 -0500

    Update Present.java

--- Present.java
+++ Present.java
@@ -1,5 +1,18 @@
+import java.util.*;
 public class Present {
     public static void Present {
+		//variables
+		Scanner reader = new Scanner (System.in);
+		int days = 0;
+		int budget = 0;
+
+		//ask santa for data
+		System.out.println("Hello, Santa!");
+		System.out.println("How many days do you have to build?");
+		days = reader.nextInt();
+		System.out.println("What is the budget this year?");
+		budget = reader.nextInt();
+
 
     }
 }
